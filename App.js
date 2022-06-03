11b0a3300c0cc4584a3b4cbcdc0ddd944511847c
unknown <gabrielsenecosta@hotmail.com>
2022-06-03 13:05:26 -0300

    v1

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+
+import Cesta from './src/telas/Cesta';
+
 
 export default function App() {
+
+ 
+
+
+  const [fonteCarregada] = useFonts({
+    "interRegular":Inter_400Regular,
+    "interBold":Inter_700Bold,
+    "MontserratRegular":Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+    
+
+  });
+
+  if(!fonteCarregada){
+    return <View/>
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   
+      <SafeAreaView>
+      <StatusBar/>
+      <Cesta/>
+      </SafeAreaView>
+      
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
